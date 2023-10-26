8c3a10c9bf471b508bcbb22cbae2aed6c907efe6
HunterGulledge4 <huntergulledgebusiness@gmail.com>
2023-10-25 23:29:30 -0500

    small changes

--- src/main/java/com/dashboard/groupfiveproject/Main.java
+++ src/main/java/com/dashboard/groupfiveproject/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class FarmDashboard extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FarmDashboard.class.getResource("farm-dashboard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("farm-dashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 1100);
         stage.setTitle("Welcome to Your AgriTech Farm Dashboard!");
         stage.setScene(scene);
