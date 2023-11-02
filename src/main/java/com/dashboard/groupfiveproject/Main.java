4cf89920896651e74a9f1ea592fe97b674f750a9
HunterGulledge4 <huntergulledgebusiness@gmail.com>
2023-11-02 10:28:30 -0500

    added fxml file but image needs fixing

--- src/main/java/com/dashboard/groupfiveproject/Main.java
+++ src/main/java/com/dashboard/groupfiveproject/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("farm-dashboard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("FarmDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 1100);
         stage.setTitle("Welcome to Your AgriTech Farm Dashboard!");
         stage.setScene(scene);
