0e7e4f49421930b6d16f0110503c0fa575cd5101
Sunny Patel <sunnypatel4prez@gmail.com>
2023-11-07 19:08:54 -0600

    Used the young indian method

--- src/main/java/com/dashboard/groupfiveproject/FarmController.java
+++ src/main/java/com/dashboard/groupfiveproject/FarmController.java
@@ -17,6 +17,9 @@ public class FarmController {
     private Rectangle commandCenter;
 
     @FXML
+    private ChoiceBox<String> crudChoiceBox;
+
+    @FXML
     private ChoiceBox<String> commandChoiceBox;
 
     @FXML
@@ -82,33 +85,119 @@ public class FarmController {
     @FXML
     private Button visitFarmItem;
 
-    public void initialize() {
-        commandChoiceBox.setItems(FXCollections.observableArrayList("Change Name", "Change Price",
-                "Change Location X", "Change Location Y", "Change Length", "Change Width", "Change Height"));
+    public void Executecommand() {
 
-        TreeItem rootItem = new TreeItem<>("Root");
-        TreeItem barnItem = new TreeItem<>("Barn");
-        TreeItem liveStockItem = new TreeItem<>("Live Stock Area");
-        TreeItem cowItem = new TreeItem<>("Cow");
-        TreeItem milkStorageItem = new TreeItem<>("Milk Storage");
-        TreeItem commandCenter = new TreeItem<>("Command Center");
-        TreeItem drone = new TreeItem<>("Drone");
-        TreeItem crops = new TreeItem<>("Crops");
-
-        treeView.setRoot(rootItem);
-
-        rootItem.getChildren().addAll(barnItem, commandCenter, crops);
-        barnItem.getChildren().addAll(liveStockItem, milkStorageItem, drone);
-        liveStockItem.getChildren().add(cowItem);
+        if (crudChoiceBox.getValue().equals("Add Item")) {
+            Additem();
+        } else if (crudChoiceBox.getValue().equals("Delete Item")) {
+            Deleteitem();
+        } else if (crudChoiceBox.getValue().equals("Add Item Container")) {
+            Additemcontainer();
+        } else if (crudChoiceBox.getValue().equals("Delete Item Container")) {
+            Deleteitemcontainer();
+        } else {
+            Changestuff();
+        }
 
     }
 
-    /*
-     * public void onSubmitButtonClicked() {
-     * String selectedCommand = commandChoiceBox.getValue();
-     * TreeItem<FarmObject> selectedItem = (TreeItem<FarmObject>)
-     * FarmObject selectedObject = selectedItem.getValue();
-     * }
-     */
+    public void Additem() {
+        String entryName = entryFieldName.getText().isBlank() ? "Left Blank" : entryFieldName.getText();
+        float entryPrice = Float.parseFloat(entryFieldPrice.getText().isBlank() ? "0" : entryFieldPrice.getText());
+        int entryLocationX = Integer
+                .parseInt(entryFieldLocationX.getText().isBlank() ? "0" : entryFieldLocationX.getText());
+        int entryLocationY = Integer
+                .parseInt(entryFieldLocationY.getText().isBlank() ? "0" : entryFieldLocationY.getText());
+        int entryLength = Integer.parseInt(entryFieldLength.getText().isBlank() ? "0" : entryFieldLength.getText());
+        int entryWidth = Integer.parseInt(entryFieldWidth.getText().isBlank() ? "0" : entryFieldWidth.getText());
+        int entryHeight = Integer.parseInt(entryFieldHeight.getText().isBlank() ? "0" : entryFieldHeight.getText());
 
+        TreeItem<FarmObject> selectedItem = treeView.getSelectionModel().getSelectedItem();
+
+        FarmObject farmObject = new FarmObject(entryName, entryPrice, entryLocationX, entryLocationY, entryLength,
+                entryWidth, entryHeight);
+
+        TreeItem<FarmObject> object = new TreeItem<FarmObject>(farmObject);
+        selectedItem.getChildren().add(object);
+
+    }
+
+    public void Deleteitem() {
+        TreeItem<FarmObject> selectedItem = treeView.getSelectionModel().getSelectedItem();
+        selectedItem.getParent().getChildren().remove(selectedItem);
+    }
+
+    public void Additemcontainer() {
+        String entryName = entryFieldName.getText().isBlank() ? "Left Blank" : entryFieldName.getText();
+        float entryPrice = Float.parseFloat(entryFieldPrice.getText().isBlank() ? "0" : entryFieldPrice.getText());
+        int entryLocationX = Integer
+                .parseInt(entryFieldLocationX.getText().isBlank() ? "0" : entryFieldLocationX.getText());
+        int entryLocationY = Integer
+                .parseInt(entryFieldLocationY.getText().isBlank() ? "0" : entryFieldLocationY.getText());
+        int entryLength = Integer.parseInt(entryFieldLength.getText().isBlank() ? "0" : entryFieldLength.getText());
+        int entryWidth = Integer.parseInt(entryFieldWidth.getText().isBlank() ? "0" : entryFieldWidth.getText());
+        int entryHeight = Integer.parseInt(entryFieldHeight.getText().isBlank() ? "0" : entryFieldHeight.getText());
+
+        TreeItem<FarmObject> selectedItem = treeView.getSelectionModel().getSelectedItem();
+
+        FarmObject farmObject = new FarmObject(entryName, entryPrice, entryLocationX, entryLocationY, entryLength,
+                entryWidth, entryHeight);
+
+        TreeItem<FarmObject> object = new TreeItem<FarmObject>(farmObject);
+        selectedItem.getChildren().add(object);
+    }
+
+    public void Deleteitemcontainer() {
+        TreeItem<FarmObject> selectedItem = treeView.getSelectionModel().getSelectedItem();
+        selectedItem.getParent().getChildren().remove(selectedItem);
+    }
+
+    public void Changestuff() {
+        TreeItem<FarmObject> selectedItem = treeView.getSelectionModel().getSelectedItem();
+
+        if (!entryFieldName.getText().isBlank()) {
+            selectedItem.getValue().setName(entryFieldName.getText());
+            System.out.println(!entryFieldName.getText().isBlank());
+        }
+        if (!entryFieldPrice.getText().isBlank()) {
+            selectedItem.getValue().setPrice(Float.parseFloat(entryFieldPrice.getText()));
+        }
+        if (!entryFieldLocationX.getText().isBlank()) {
+            selectedItem.getValue().setLocationX(Integer.parseInt(entryFieldLocationX.getText()));
+        }
+        if (!entryFieldLocationY.getText().isBlank()) {
+            selectedItem.getValue().setLocationY(Integer.parseInt(entryFieldLocationY.getText()));
+        }
+        if (!entryFieldLength.getText().isBlank()) {
+            selectedItem.getValue().setLength(Integer.parseInt(entryFieldLength.getText()));
+        }
+        if (!entryFieldWidth.getText().isBlank()) {
+            selectedItem.getValue().setWidth(Integer.parseInt(entryFieldWidth.getText()));
+        }
+        if (!entryFieldHeight.getText().isBlank()) {
+            selectedItem.getValue().setHeight(Integer.parseInt(entryFieldHeight.getText()));
+        }
+
+    }
+
+    public void initialize() {
+        // Initialize the root TreeItem and set it as the root of the TreeView
+        TreeItem<FarmObject> rootItem = new TreeItem<>(new FarmObject("Root", 0, 0, 0, 0, 0, 0)); // You can set default
+                                                                                                  // values
+        treeView.setRoot(rootItem);
+
+        // Set a custom CellFactory to display the name of FarmObjects
+        treeView.setCellFactory(param -> new TreeCell<FarmObject>() {
+            @Override
+            protected void updateItem(FarmObject item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName()); // Display the name of the FarmObject
+                }
+            }
+        });
+    }
 }
